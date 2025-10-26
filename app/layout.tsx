16bb6ef39805e32376486a7359e5466609483b2d
bilals2008 <wbiz2939@gmail.com>
2025-10-26 10:19:25 +0500

    adding dark theme for default

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
