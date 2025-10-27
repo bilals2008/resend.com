8daba5ddea15cc69617c9283d3e49c9fa81b2bcc
bilals2008 <wbiz2939@gmail.com>
2025-10-27 08:56:28 +0500

    feat: implement FeatureCard component with dynamic features list and icons

---
+++ components/DeveloperExperience/index.ts
@@ -0,0 +1,18 @@
+// File: components/DeveloperExperience/index.ts
+
+export const featuresList = [
+  {
+    imgSrc: "/images/DevExp/img-1.svg",
+    altText: "Test Mode Image",
+    icons: "FlaskConical",
+    heading: "Test Mode",
+    para: "Simulate events and experiment with our API without the risk of accidentally sending real emails to real people.",
+  },
+  {
+    imgSrc: "/images/DevExp/img-2.svg",
+    altText: "Modular Webhooks Image",
+    icons: "FlaskRound",
+    heading: "Modular Webhooks",
+    para: " Receive real-time notifications directly to your server. Every time an email is delivered, opened, bounces, or a link is clicked",
+  },
+];
