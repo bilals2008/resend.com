df92db4a0c6484f70e14056dc07d246b1db240a1
bilals2008 <wbiz2939@gmail.com>
2025-10-28 10:47:55 +0500

    add ourStory  component

--- app/about/AboutIntro.tsx
+++ app/about/AboutIntro.tsx
@@ -1,10 +1,12 @@
 // File: app/about/AboutIntro.tsx
 import ContentSection from '@/components/common/ContentSection';
+import ShiningLine from '@/components/common/ShiningLine';
 import React from 'react'
+import OurStory from './OurStory';
 
 function AboutIntro() {
   return (
-    <div className="max-w-5xl mx-auto py-5">
+    <div className="max-w-5xl mx-auto py-5 ">
       <ContentSection
         imgSrc="/images/About/aboutIntro.png"
         imgalt="aboutIntro image"
@@ -15,6 +17,10 @@ email sending platform"
           "The web has come a long way in the last ten years, but why is email stuck in the past? It doesn't have to be that way. We want to change that. We want to reimagine email."
         }
       />
+      <div className="relative">
+        <ShiningLine />
+      </div>
+      <OurStory />
     </div>
   );
 }
---
+++ app/about/OurStory.tsx
@@ -0,0 +1,32 @@
+// File: app/about/OurStory.tsx
+import React from 'react'
+
+function OurStory() {
+  return (
+    <div className="py-10">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl py-10 text-center">
+        OurStory
+      </h1>
+      <div className="max-w-lg sm:max-w-xl md:max-w-2xl tracking-wide paraColor mx-auto text-base sm:text-lg md:text-[18px] space-y-3 sm:space-y-4 md:space-y-5 leading-7 sm:leading-8 font-light align-middle">
+        <p>
+          Resend started with an open source project in 2022. We were frustrated
+          by how difficult it was to build modern email templates that worked
+          well across all email clients.
+        </p>
+        <p>
+          As we started to dig deeper, it became clear that this was just the
+          tip of the iceberg. We realized that sending emails that reach the
+          inbox (not the spam folder) was the biggest pain point, so we started
+          to explore something new.
+        </p>
+        <p>
+          In 2023, we launched an entire email sending platform and joined Y
+          Combinator&apos;s winter batch. We&apos;re creating something special
+          here, and we&apos;re excited to build it with you.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default OurStory
