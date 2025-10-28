3ec8b82b719999958ed886f673db8d3e00abf2b7
bilals2008 <wbiz2939@gmail.com>
2025-10-28 20:14:49 +0500

    fix images path for companies logos section

--- app/Home/Companies/logos.ts
+++ app/Home/Companies/logos.ts
@@ -6,51 +6,51 @@ type Logos = {
 
 export const logos: Logos[] = [
   {
-    src: "/images/companies/logo-1.png",
+    src: "/images/Home/companies/logo-1.png",
     alt: "Brand-1 image",
   },
   {
-    src: "/images/companies/logo-2.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-2.png",
+    alt: "Brand-2 image",
   },
   {
-    src: "/images/companies/logo-3.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-3.png",
+    alt: "Brand-3 image",
   },
   {
-    src: "/images/companies/logo-4.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-4.png",
+    alt: "Brand-4 image",
   },
   {
-    src: "/images/companies/logo-5.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-5.png",
+    alt: "Brand-5 image",
   },
   {
-    src: "/images/companies/logo-6.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-6.png",
+    alt: "Brand-6 image",
   },
   {
-    src: "/images/companies/logo-7.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-7.png",
+    alt: "Brand-7 image",
   },
   {
-    src: "/images/companies/logo-8.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-8.png",
+    alt: "Brand-8 image",
   },
   {
-    src: "/images/companies/logo-9.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-9.png",
+    alt: "Brand-9 image",
   },
   {
-    src: "/images/companies/logo-10.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-10.png",
+    alt: "Brand-10 image",
   },
   {
-    src: "/images/companies/logo-11.png",
-    alt: "Brand-1 image",
+    src: "/images/Home/companies/logo-11.png",
+    alt: "Brand-11 image",
   },
   {
     src: "/images/companies/logo-12.png",
-    alt: "Brand-1 image",
+    alt: "Brand-12 image",
   },
 ];
