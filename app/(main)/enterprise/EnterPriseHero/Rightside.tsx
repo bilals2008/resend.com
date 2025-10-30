405c0f9ae1131dfd84ca2014ed7535cd9df612c1
bilals2008 <wbiz2939@gmail.com>
2025-10-30 16:59:17 +0500

    imrove some ui for sm screens

--- app/(main)/enterprise/EnterPriseHero/Rightside.tsx
+++ app/(main)/enterprise/EnterPriseHero/Rightside.tsx
@@ -11,12 +11,12 @@ sending volume"
         desciption="It doesn't matter if you send millions or hundreds of emails every
 month. You need every guarantee that your messages will arrive
 on time, that your data will be safely stored, and always available."
-        headingClassName=" order-2 text-4xl text-balance text-start"
-        paraClassName=" order-3 text-balance text-start"
-        btn1ClassName="order-1 my-2 bg-background border text-white hover:text-black"
+        headingClassName=" order-2 text-4xl text-balance md:text-start"
+        paraClassName=" order-3 text-balance md:text-start"
+        btn1ClassName="order-1 my-2 bg-background border text-white mb-3 hover:text-black"
         btnText={["Enterprise"]}
         btnWrapperClassName="justify-start!"
-        ClassName="items-start"
+        ClassName=" md:items-start"
       />
     </div>
   );
