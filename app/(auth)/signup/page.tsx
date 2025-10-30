4406fc5df0629e5ae3eedab59b4233e332518cb9
bilals2008 <wbiz2939@gmail.com>
2025-10-30 10:12:43 +0500

    add signup page fix routes confilicts because of two same routes

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,19 +1,20 @@
-// File: app/(auth)/login/page.tsx
-import { LoginForm } from "@/components/ui/login-form";
+// File: app/(auth)/register/page.tsx
+import { SignupForm } from "@/components/ui/signup-form";
 import Link from "next/link";
 import React from "react";
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6">
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center">
+     
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link
-          href="/"
+          href={"/"}
           className="flex items-center justify-center gap-2 ml-6 text-sm size-10 rounded-md p-2 border-2 text-white font-bold hover:bg-muted/50"
         >
           R
         </Link>
-        <LoginForm className="bg-background" />
+        <SignupForm className="bg-background" />
       </div>
     </div>
   );
