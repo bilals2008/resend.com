9275fc5ece69a9e9a82b699a0b9ef5d2ce9d38aa
bilals2008 <wbiz2939@gmail.com>
2025-10-30 16:12:58 +0500

    add loading component

---
+++ app/(auth)/loading.tsx
@@ -0,0 +1,12 @@
+// File: app/(auth)/loading.tsx
+import React from 'react'
+
+function loading() {
+  return (
+    <div className="w-screen h-screen z-10">
+      <span className="loader"></span>
+    </div>
+  );
+}
+
+export default loading
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -6,7 +6,6 @@ import React from "react";
 export default function Page() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center">
-     
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link
           href={"/"}
